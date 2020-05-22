71f53cbcb63d260c723c51d0df7ba02a9aba7433
Emma Lubes <eml5244@rit.edu>
2020-05-21 21:30:18 -0600

    Rough drafts of the iterative spiral drawing programs

--- CS1/Iteration/Iteration_Solution_Version.py
+++ CS1/Iteration/Iteration_Solution_Version.py
@@ -3,81 +3,55 @@ A iterative turtle drawing solution to learn how to work on iterative skills and
 Created by Emma Lubes, eml5244, for the Academic Success Center Supplemental Instruction Program.
 """
 import turtle
+import random
+
+"""
+A function which uses the random package to generate 3 random integers for RGB colors and returns it as a tuple which 
+is then passed into turtle.color() to change the color of the line segement
+"""
+def ran_color_gen():
+    color1 = random.randrange(0, 256)
+    color2 = random.randrange(0, 256)
+    color3 = random.randrange(0, 256)
+    return color1, color2, color3
 
 
-# Draw depth of 1
-def draw_depth_1(length):
-    # TODO: Start the color fill and put the pen down
-    turtle.begin_fill()
-    turtle.down()
-    # TODO: Draw the triangle (Hint: make sure to return to where you started drawing the triangle!)
-    turtle.forward(length)
-    turtle.right(120)
-    turtle.forward(length)
-    turtle.right(120)
-    turtle.forward(length)
-    turtle.right(120)
-    # TODO: Pick the pen up and end the fill color
-    turtle.up()
-    turtle.end_fill()
-
-
-# Finish this recursive function!
-def draw_rec(length, depth):
-    # TODO: Create the base case
-    if depth == 1:
-        draw_depth_1(length)
-    # TODO: Finish the recursive case
-    # Hint: Enter the correct calls to make the function recursive!
+# Make the tail recursive function
+def draw_tail_rec(length):
+    # TODO: Make the base case
+    if length == 0:
+        pass
+    # TODO: Make the recursive case
     else:
-        draw_rec(length, 1)
-        turtle.forward(length/2)
-        turtle.left(120)
-        turtle.forward(length/2)
-        turtle.right(120)
-        draw_rec(length/2, depth-1)
-        turtle.right(60)
-        turtle.forward(length/2)
-        turtle.left(60)
-        turtle.forward(length/2)
-        turtle.right(120)
-        turtle.forward(length/2)
-        turtle.left(120)
-        draw_rec(length/2, depth-1)
-        turtle.right(120)
-        turtle.forward(length/2)
-        turtle.right(120)
-        turtle.forward(length/2)
-        turtle.left(60)
-        turtle.forward(length/2)
-        turtle.left(180)
-        draw_rec(length/2, depth-1)
-        turtle.forward(length/2)
-        turtle.left(120)
-        turtle.forward(length/2)
-        turtle.right(120)
+        turtle.forward(length)
+        turtle.left(90)
+        tup = ran_color_gen()
+        turtle.color(tup)
+        draw_tail_rec(length-1)
 
 
-def draw_tail_rec(length, depth):
-    if depth == 0:
-        pass
-    else:
-        pass
-
-
-def draw_iter(length, depth):
-    while depth > 0:
-        pass
+# Make the recursive function
+def draw_iter(length):
+    # TODO: Set up the loop
+    while length > 0:
+        tup = ran_color_gen()
+        turtle.color(tup)
+        # TODO: Draw the spiral
+        turtle.forward(length)
+        turtle.left(90)
+        length -= 1
+        turtle.color(tup)
 
 
 def main():
     # TODO: Get a length distance from the user
     length = int(input("Enter a length: "))
-    # TODO: Get a recursive depth from the user
-    depth = int(input("Enter a recursive depth: "))
     turtle.speed(0)
-    turtle.fillcolor("black")
+    turtle.colormode(255)
     # TODO Make the iterative call
-    draw_rec(length, depth)
+    draw_iter(length)
+    turtle.hideturtle()
     turtle.done()
 
+
+main()
--- CS1/Iteration/Iteration_Student_Version.py
+++ CS1/Iteration/Iteration_Student_Version.py
@@ -2,5 +2,46 @@
 A iterative turtle drawing to learn how to work on iterative skills and move from recursion to iteration.
 Created by Emma Lubes, eml5244, for the Academic Success Center Supplemental Instruction Program.
 """
+import turtle
+import random
+
+"""
+A function which uses the random package to generate 3 random integers for RGB colors and returns it as a tuple which 
+is then passed into turtle.color() to change the color of the line segement
+"""
+def ran_color_gen():
+    color1 = random.randrange(0, 256)
+    color2 = random.randrange(0, 256)
+    color3 = random.randrange(0, 256)
+    return color1, color2, color3
 
 
+# Make the tail recursive function
+def draw_tail_rec(length):
+    # TODO: Make the base case
+    # TODO: Make the recursive case
+    # Hint: These color changing lines should go right before the end of the function!
+        tup = ran_color_gen()
+        turtle.color(tup)
+
+
+# Make the recursive function
+def draw_iter(length):
+    # TODO: Set up the loop
+        tup = ran_color_gen()
+        turtle.color(tup)
+        # TODO: Draw the spiral
+
+
+
+def main():
+    # TODO: Get a length distance from the user
+    length = None
+    turtle.speed(0)
+    turtle.colormode(255)
+    # TODO Make the iterative call
+
+    turtle.hideturtle()
+    turtle.done()
+
+main()
