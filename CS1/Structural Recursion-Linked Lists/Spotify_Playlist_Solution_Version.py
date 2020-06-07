e3653e9edaea9c8faf95d4b6f6decb71f8bffff3
Emma Lubes <eml5244@rit.edu>
2020-06-07 14:59:55 -0600

    Solution Version file creation and problem description in comments

---
+++ CS1/Structural Recursion-Linked Lists/Spotify_Playlist_Solution_Version.py
@@ -0,0 +1,9 @@
+"""
+As a software developer at Spotify, you've been asked to re-implement their playlist structure into a Linked List
+structure. You've been told that along with displaying the current song, listeners must also be able to go back a song
+and skip to the next song.
+
+This is the solution version for the Spotify Playlist.
+Created by Emma Lubes, eml5244, for the Academic Success Center Supplemental Instruction Program.
+"""
+
