72d577bd372f28cd4a684b22a3aae6d6e0b8d1fb
yxd3549 <yxd3549@rit.edu>
2020-06-17 20:39:49 -0400

    add kenken backtracking exercise

---
+++ CS2/Backtracking/Coordinate.java
@@ -0,0 +1,24 @@
+package Backtracking;
+
+/**
+ * This class is used to represent a coordinate in a grid.
+ * Simply put, it's just two numbers
+ */
+public class Coordinate {
+
+    private int row;
+    private int col;
+
+    public Coordinate(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
---
+++ CS2/Backtracking/Operation.java
@@ -0,0 +1,9 @@
+package Backtracking;
+
+/**
+ * An Enum to represent the operations we can support in our puzzle
+ */
+public enum Operation {
+    SUM,
+    SUBTRACTION,
+}
