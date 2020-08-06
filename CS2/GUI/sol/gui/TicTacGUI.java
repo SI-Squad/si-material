174aa8ec3b18c348a06a03c7a994287df81e2f82
Emma Lubes <eml5244@rit.edu>
2020-08-05 21:00:28 -0400

    Wrote out the Tic Tac Toe structure and implemented the CS Observer interface along with the TicTacProtocol which is based off the CS ConnectFourProtocol interface

---
+++ CS2/GUI/sol/gui/TicTacGUI.java
@@ -0,0 +1,7 @@
+package GUI.sol.gui;
+
+/**
+ A Java GUI for this networked Tic Tac Toe Game.
+ Created by Emma Lubes, eml5244, for the Academic Success Center Supplemental Instruction Program.
+ */
+
